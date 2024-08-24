cc307412efd6bfb8b0e61eed017f5a6f4a759439
sriramreddy003 <viswavikayathasriramreddy@gmail.com>
2024-08-24 14:20:40 -0400

    added two files

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo
+echo please choose one of the option below
+echo
+echo 'a = Display date and time'
+echo
+echo 'b = list files and directories'
+echo
+echo 'c = list user logged in'
+echo
+echo 'd = check system update'
+echo
+	read $choices
+	case $choices in
+a) date;;
+b) ls -l;;
+c) touch bangaru.txt;;
+d) uptime;;
+e) echo "Hello World";;
+f) echo invalid choices
+		esac 
